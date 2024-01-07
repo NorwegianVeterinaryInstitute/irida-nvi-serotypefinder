2f0e1915ce038c14f6a3af2242b51ff66a6374cd
ajkarloss <ajkarloss@gmail.com>
2024-01-07 15:41:09 +0100

    formatting scripts for serotrype finder raw output

---
+++ format_script_for_galaxy/format_serotyper_results.py
@@ -0,0 +1,31 @@
+import sys
+import re
+
+OF = open("formatted_serotype_results.tsv","w")
+
+F1 = open(sys.argv[1],"r")
+h_type = "NIL"
+o_type = "NIL"
+for i in F1:
+    temp = i.split()
+    if re.search("H_type", temp[0]):
+        h_type = temp[2]
+    if re.search("O_type",temp[0]):
+        o_type = temp[2]
+
+if h_type != "NIL" and o_type != "NIL":
+    serotype = o_type + ":" + h_type
+elif h_type != "NIL" and o_type == "NIL":
+    serotype = o_type + ":" + "HNT"
+elif h_type == "NIL" and o_type != "NIL":
+    serotype = "ONT" + ":" + h_type
+else: 
+    serotype = "ONT" + ":" + "HNT"
+
+F1.close()
+
+OF.write("Serotype\n")
+OF.write(serotype + "\n")
+
+
+OF.close()
