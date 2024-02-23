ddecf9d1125416afbfa71ff9746c3e1703f805fe
Jeevan Karloss <ajkarloss@gmail.com>
2024-02-23 13:38:57 +0100

    Update format_serotyper_results.py
    Modified the output format

--- format_script_for_galaxy/format_serotyper_results.py
+++ format_script_for_galaxy/format_serotyper_results.py
@@ -6,12 +6,18 @@ OF = open("formatted_serotype_results.tsv","w")
 F1 = open(sys.argv[1],"r")
 h_type = "NIL"
 o_type = "NIL"
+n = 0
 for i in F1:
     temp = i.split()
     if re.search("H_type", temp[0]):
         h_type = temp[2]
     if re.search("O_type",temp[0]):
-        o_type = temp[2]
+        if n == 0:
+            o_type = temp[2]
+        else:
+            o_type = o_type + "/" + temp[2]
+        n = n + 1
+        #o_type = temp[2]
 
 if h_type != "NIL" and o_type != "NIL":
     serotype = o_type + ":" + h_type
